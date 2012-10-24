4aa04fe3b1745e00e5bbb36dda7816c8ad93cf81
bboc <bboc@gmx.net>
2012-10-24 11:57:08 +0200

    write binary files, changed tabs to spaces, added CloseInvoice

--- client.py
+++ client.py
@@ -2,92 +2,96 @@ import socket
 import uuid
 
 class SocketManager(object):
-	"""
-	Context manager for the actual socket, so we make sure that the 
-	socket is closed
-	"""
-	def __init__(self, host, port):
-		self.host = host
-		self.port = port
+    """
+    Context manager for the actual socket, so we make sure that the 
+    socket is closed
+    """
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
-	def __enter__(self):
-		self.s = socket.create_connection((self.host, self.port),5)
-		return self.s
+    def __enter__(self):
+        self.s = socket.create_connection((self.host, self.port),5)
+        return self.s
 
-	def __exit__(self, type, value, traceback):
-		self.s.close()
+    def __exit__(self, type, value, traceback):
+        self.s.close()
 
 
 class ProtelClient(object):
-	"""Handle requests and responses to the protel server."""
+    """Handle requests and responses to the protel server."""
 
-	def __init__(self, method, headers, body, host=None, port=None):
-		self._method = method
-		self._headers = headers
-		self._body = body
-		self._host = host
-		self._port = port
+    def __init__(self, method, headers, body, host=None, port=None):
+        self._method = method
+        self._headers = headers
+        self._body = body
+        self._host = host
+        self._port = port
 
-	@property
-	def request(self):
-		return ''.join(list((self.header, "\r\n", self._body)))
+    @property
+    def request(self):
+        return ''.join(list((self.header, "\r\n", self._body)))
 
-	@property
-	def header(self):
+    @property
+    def header(self):
 
-		# create header
-		hdr = list()
-		hdr.append("POST {} HTTP/1.1".format(self._method))
-		for key in self._headers:
-			hdr.append("{}: {}".format(key, self._headers[key]))
-		hdr.append("Content-length: {}".format(len(self._body)))
-		hdr.append('') # end of header
-		return "\r\n".join(hdr)
+        # create header
+        hdr = list()
+        hdr.append("POST {} HTTP/1.1".format(self._method))
+        for key in self._headers:
+            hdr.append("{}: {}".format(key, self._headers[key]))
+        hdr.append("Content-length: {}".format(len(self._body)))
+        hdr.append('') # end of header
+        return "\r\n".join(hdr)
 
-	def send(self):
+    def send(self):
 
+        def get_response(s):
+            SIZE = 1024
+            BODY_SEPARATOR = '\r\n\r\n'
+            response = s.recv(SIZE)
+            raw_header, sep, body = response.partition(BODY_SEPARATOR)
+            # if not body:
+            #   raise Exception('malformed response {}'.format(repr(response)))
+            
+            header = split_header(raw_header)
+            try:
+                # close invoice has not content length and no body :-(
+                content_length = header['Content-length']
+            except KeyError:
+                return response
 
-		def get_response(s):
-			SIZE = 1024
-			BODY_SEPARATOR = '\r\n\r\n'
-			response = s.recv(SIZE)
-			raw_header, sep, body = response.partition(BODY_SEPARATOR)
-			if not body:
-				raise Exception('malformed response {}'.format(repr(response)))
-			
-			header = split_header(raw_header)
-			content_length = header['Content-length']
-			while len(body) < int(content_length):
-				body +=	s.recv(SIZE)
-			return BODY_SEPARATOR.join((raw_header, body))
+            while len(body) < int(content_length):
+                body += s.recv(SIZE)
+            return BODY_SEPARATOR.join((raw_header, body))
 
-		def split_header(raw_header):
-			"""Split a header into a dictionary"""
-			lines = raw_header.split('\r\n')[1:]
-			header = dict()
-			for line in lines:
-				key, sep, value = line.partition(':')
-				header[key] = value.strip()
-			return header
+        def split_header(raw_header):
+            """Split a header into a dictionary"""
+            lines = raw_header.split('\r\n')[1:]
+            header = dict()
+            for line in lines:
+                key, sep, value = line.partition(':')
+                header[key] = value.strip()
+            return header
 
-		with SocketManager(self._host, self._port) as s:
-			result = s.sendall(self.request)
-			if result == None:
-				# send successful
-				return get_response(s)
-			else:
-				print "### error while sending ###"
+        with SocketManager(self._host, self._port) as s:
+            result = s.sendall(self.request)
+            if result == None:
+                # send successful
+                return get_response(s)
+            else:
+                print "### error while sending ###"
 
 
 def protel_request(method, host, port, body='', headers=None, outlet='1',transaction=None):
 
-	if transaction == None:
-		transaction = str(uuid.uuid1().int)[:9] # protel only supports 9 digits ;-)
-	if headers == None:
-		headers = dict()
+    if transaction == None:
+        transaction = str(uuid.uuid1().int)[:9] # protel only supports 9 digits ;-)
+    if headers == None:
+        headers = dict()
 
-	headers['Transaction'] = transaction
-	headers['Outlet'] = outlet
+    headers['Transaction'] = transaction
+    headers['Outlet'] = outlet
 
-	r = ProtelClient(method, headers, body, host, port)
-	return r.request, r.send()
+    r = ProtelClient(method, headers, body, host, port)
+    return r.request, r.send()
--- demo_requests.py
+++ demo_requests.py
@@ -1,92 +1,155 @@
 import argparse
 from functools import partial
-
+import textwrap
 from client import protel_request
 
 
 def parse_args():
-	parser = argparse.ArgumentParser(description='Send requests to protel test server and print or save responses')
-	parser.add_argument('-t', '--target', choices=['file', 'stdout'], default='stdout',
-						help='what to do with the output (file|stdout)')
-	parser.add_argument('host', help='server host')
-	parser.add_argument('port', help='server port')
+    parser = argparse.ArgumentParser(description='Send requests to protel test server and print or save responses')
+    parser.add_argument('-t', '--target', choices=['file', 'stdout'], default='stdout',
+                        help='what to do with the output (file|stdout)')
+    parser.add_argument('host', help='server host')
+    parser.add_argument('port', help='server port')
 
-	return parser.parse_args()
+    return parser.parse_args()
 
 
 def to_stdout(title, request, response):
-	print "#"*len(title)
-	print title
-	print "#"*len(title)
-	print '------------ Request ------------\n', str(request)
-	print '------------ Response------------\n', str(response)
+    print "#"*len(title)
+    print title
+    print "#"*len(title)
+    print '------------ Request ------------\n', str(request)
+    print '------------ Response------------\n', str(response)
 
 def to_file(title, request, response):
 
-	print "writing files for {}".format(title)
-	def write(title, kind, data):
-		with file('examples/{}-{}'.format(title, kind), 'w+') as f:
-			f.write(data)
-	write(title, 'request', request)
-	write(title, 'response', response)
+    print "writing files for {}".format(title)
+    def write(title, kind, data):
+        with file('examples/{}-{}'.format(title, kind), 'wb+') as f:
+            f.write(data)
+    write(title, 'request', request)
+    write(title, 'response', response)
 
 FILE = to_file
 STDOUT = to_stdout
 
 def demo_request(title, method, body='', headers=None, outlet='1', transaction=None, host=None, port=None, target=STDOUT):
 
-	request, response = protel_request(method, host, port, body, headers, outlet, transaction)	
-	target(title, request, response)
+    request, response = protel_request(method, host, port, body, headers, outlet, transaction)  
+    target(title, request, response)
 
 
 def demo_requests(target, host, port):
 
-	req = partial(demo_request, target=target, host=host, port=port)
+    req = partial(demo_request, target=target, host=host, port=port)
 
-	req('ValidateReservation with invalid ResNo',
-		 'ValidateReservation', 
-		 "<Body><ResNo>5</ResNo></Body>")
+    req('CloseInvoice (simple)',
+         'CloseInvoice', 
+         textwrap.dedent("""<Body>
+            <Creation>20121020164510</Creation>
+            <Outlet>1</Outlet>
+            <User>1</User>
+            <Invoice>124</Invoice>
+            <Item>
+                <Type>Revenue</Type>
+                <Productgroup>1</Productgroup>
+                <TaxClassification>2</TaxClassification>
+                <TotalAmount>2.50</TotalAmount>
+            </Item>
+            <Item>
+                <Type>Payment</Type>
+                <TotalAmount>2.50</TotalAmount>
+                <ResNo>533</ResNo>
+            </Item>
+            </Body>"""))
+
+    req('CloseInvoice (complex with expenses and gratuity)',
+         'CloseInvoice', 
+         textwrap.dedent("""<Body>
+            <Creation>20121020164510</Creation>
+            <Outlet>1</Outlet>
+            <User>1</User>
+            <Invoice>123</Invoice>
+            <Item>
+                <Type>Revenue</Type>
+                <Text>Astra</Text>
+                <Article>115</Article>
+                <Productgroup>1</Productgroup>
+                <TaxClassification>2</TaxClassification>
+                <SingleAmount>2.50</SingleAmount>
+                <Quantity>3</Quantity>
+                <TotalAmount>7.50</TotalAmount>
+            </Item>
+            <Item>
+                <Type>Revenue</Type>
+                <Text>Otternasen im Schlafrock</Text>
+                <Article>212</Article>
+                <Productgroup>9</Productgroup>
+                <TaxClassification>2</TaxClassification>
+                <SingleAmount>12.50</SingleAmount>
+                <Quantity>1</Quantity>
+                <TotalAmount>12.50</TotalAmount>
+            </Item>
+            <Item>
+                <Type>Gratuity</Type>
+                <TotalAmount>0.50</TotalAmount>
+            </Item>
+            <Item>
+                <Type>Expense</Type>
+                <Text>Zigarren</Text>
+                <TotalAmount>300</TotalAmount>
+            </Item>
+            <Item>
+                <Type>Payment</Type>
+                <TotalAmount>320.50</TotalAmount>
+                <ResNo>533</ResNo>
+            </Item>
+            </Body>"""))
+
+    req('ValidateReservation with invalid ResNo',
+         'ValidateReservation', 
+         "<Body><ResNo>5</ResNo></Body>")
 
 
-	req('ValidateReservation with valid ResNo',
-		 'ValidateReservation', 
-		 "<Body><ResNo>533</ResNo></Body>")
+    req('ValidateReservation with valid ResNo',
+         'ValidateReservation', 
+         "<Body><ResNo>533</ResNo></Body>")
 
 
-	req('FindReservationByName - name does not exist', 
-		'FindReservationByName', 
-		"<Body><Search>Protel</Search></Body>")
+    req('FindReservationByName - name does not exist', 
+        'FindReservationByName', 
+        "<Body><Search>Protel</Search></Body>")
 
-	req('FindReservationByName - name exists', 
-		'FindReservationByName', 
-		"<Body><Search>Bockelbrink</Search></Body>")
+    req('FindReservationByName - name exists', 
+        'FindReservationByName', 
+        "<Body><Search>Bockelbrink</Search></Body>")
 
-	req('FindReservationByRoom - room does not exist',
-		'FindReservationByRoom',
-	 	"<Body><Room>1408</Room></Body>")
-
-
-	req('FindReservationByRoom - room exists (108)',
-		'FindReservationByRoom',
-	 	"<Body><Room>108</Room></Body>")
-
-	req('FindReservationByRoom - room exists (109)',
-		'FindReservationByRoom',
-	 	"<Body><Room>109</Room></Body>")
+    req('FindReservationByRoom - room does not exist',
+        'FindReservationByRoom',
+        "<Body><Room>1408</Room></Body>")
 
 
-	for i in range(97, 97+26):
-		req('FindReservationByName - name exists ({})'.format(chr(i)), 
-			'FindReservationByName', 
-			"<Body><Search>{}</Search></Body>".format(chr(i)))
+    req('FindReservationByRoom - room exists (108)',
+        'FindReservationByRoom',
+        "<Body><Room>108</Room></Body>")
+
+    req('FindReservationByRoom - room exists (109)',
+        'FindReservationByRoom',
+        "<Body><Room>109</Room></Body>")
 
 
-if __name__ == "__main__":	
-	args = parse_args()	
+    for i in range(97, 97+26):
+        req('FindReservationByName - name exists ({})'.format(chr(i)), 
+            'FindReservationByName', 
+            "<Body><Search>{}</Search></Body>".format(chr(i)))
 
-	if args.target == 'file':
-		target = to_file
-	else:
-		target = to_stdout
 
-	demo_requests(target, args.host, args.port)
+if __name__ == "__main__":  
+    args = parse_args() 
+
+    if args.target == 'file':
+        target = to_file
+    else:
+        target = to_stdout
+
+    demo_requests(target, args.host, args.port)
